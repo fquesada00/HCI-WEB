149cd5638145d0a3526d3ec8b5bfd92214cf70db
segundoespina <espina.segundoo@gmail.com>
2020-10-22 20:01:14 -0300

    header variante

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -19,9 +19,8 @@
     <v-toolbar-title class="display-2" @click="$router.push('/')"
                      style="cursor:pointer; width: 500px" >FitBo</v-toolbar-title>
     <v-tabs right fixed-tabs>
-      <v-tab v-for="tab in tabs" v-bind:key="tab.name" :to="tab.route" >
-        <div>{{ tab.name }}</div>
-        <v-icon v-show="tab.icon != null" right>{{ tab.icon }}</v-icon>
+      <v-tab v-for="tab in filteredTabs" v-bind:key="tab.name" :to="tab.route" >
+        <div :append-icon="tab.icon" v-if="tab.showme">{{ tab.name }}</div>
       </v-tab>
     </v-tabs>
   </v-app-bar>
@@ -31,25 +30,40 @@ export default {
   name: "Header.vue",
 
   data: () => ({
+    loggedIn: false,
     tabs: [
-      {name: "Inicio", icon: null, bar: false, route: "/"},
-      {name: "Crear Rutinas", icon: null, bar: false, route: "/rutinas"},
-      {name: "Explorar", icon: null, bar: false, route: "/explore"},
-      {name: "Mi Perfil", icon: null, bar: true, route: "/profile"},
+      {name: "Inicio", icon: null, showme: true,route: "/"},
+      {name: "Crear Rutinas", icon: null, showme: true, route: "/rutinas"},
+      {name: "Explorar", icon: null, showme: true, route: "/explore"},
+      {name: "Mi Perfil", icon: null, showme: loggedIn, route: "/profile"},
       {
         name: "Iniciar Sesion",
         icon: "mdi-login",
-        bar: false,
+        showme: !loggedIn,
         route: "/login",
       },
       {
         name: "Crear Cuenta",
         icon: "mdi-account-plus-outline",
-        bar: false,
+        showme: !loggedIn,
         route: "/signin",
+      },
+      {
+        name: "Cerrar Sesion",
+        icon: "mdi-exit-to-app",
+        showme: loggedIn,
+        route: "/",
       },
     ],
     active: 0,
   }),
+  computed: {
+    filteredTabs: function() {
+      return this.tabs.filter(e => e.showme)
+    },
+    loggedIn: function() {
+      return sessionStorage.getItem('token') != null
+    }
+  }
 };
 </script>
