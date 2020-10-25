a8d22d63280220d7ece385022fbee4d2a4623122
segundoespina <espina.segundoo@gmail.com>
2020-10-25 02:41:10 -0300

    exloradores

---
+++ src/views/ExploreMisEjercicios.vue
@@ -0,0 +1,15 @@
+<template>
+  <ver-rutinas rutina="misEjercicios" nombre-vista="Mis Ejercicios"></ver-rutinas>
+</template>
+
+<script>
+import verRutinas from "../components/verRutinas";
+export default {
+  name: "Mis Ejercicios",
+  components: {verRutinas}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/ExploreMisRutinas.vue
@@ -0,0 +1,15 @@
+<template>
+  <ver-rutinas rutina="misRutinas" nombre-vista="Mis Rutinas"></ver-rutinas>
+</template>
+
+<script>
+import verRutinas from "../components/verRutinas";
+export default {
+  name: "Mis rutinas",
+  components: {verRutinas}
+}
+</script>
+
+<style scoped>
+
+</style>
